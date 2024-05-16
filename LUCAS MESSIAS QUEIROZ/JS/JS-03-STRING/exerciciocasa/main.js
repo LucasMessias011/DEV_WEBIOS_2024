a7aead073c39a85e084dcb6bd9bfbdd5ea1c33a4
LucasMessias011 <queirozlucas703@gmail.com>
2024-05-16 20:27:03 -0300

    atualizado exerciciocasa

---
+++ LUCAS MESSIAS QUEIROZ/JS/JS-03-STRING/exerciciocasa/main.js
@@ -0,0 +1,6 @@
+var string = "Estamos aprendendo JavaScript e seremos futuros Devs"
+
+console.log(string.charAt(19))
+console.log(`Essa string tem ${string.length} carecteres`)
+console.log(string.toLocaleUpperCase())
+console.log(string.substring(48));
