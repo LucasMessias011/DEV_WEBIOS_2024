41966e51933210b40cfac94a6fc678228c4e7f0a
LucasMessias011 <queirozlucas703@gmail.com>
2024-05-15 21:41:08 -0300

    aulaJS05

---
+++ LUCAS MESSIAS QUEIROZ/JS/JS-05-REPETIÇÃO/main.js
@@ -0,0 +1,24 @@
+//WHILE
+//Enquanto 0 for menor que 10 vou apresentar a mensagem "0 ainda é menor que 10"
+
+
+//var contador = 0
+
+//while(contador <= 10){
+  //  console.log("0 ainda é menor que 10")
+    //debugger
+    //contador++;
+//}
+
+var contador = 0
+
+do{
+    debugger
+    console.log(contador + " ainda é menor que 10 ")
+
+    contador++;
+}while(contador < 10)
+
+for(var ontador = 10 ; contador < 10 ; contador++){
+    console.log(contador + "ainda é menor que 10")
+}
