b59e22d3802026f6f92b4a14bfcd23156cec1758
LucasMessias011 <queirozlucas703@gmail.com>
2024-06-14 20:37:41 -0300

    Aula15-API Atualizada

--- LUCAS MESSIAS QUEIROZ/JS/JS-15-API-PARTE1/main.js
+++ LUCAS MESSIAS QUEIROZ/JS/JS-15-API-PARTE1/main.js
@@ -5,20 +5,22 @@ function getImage(e) {
     // console.log(url_api);
     // Função fetch para buscar dados na API
     fetch(url_api, {
-    method: 'GET',
+        method: 'GET',
     })
-    .then((response) => {
-    return response.json();
-    })
-    .then((data) => {
-    // console.log(data);
-    document.getElementById('json_aqui').innerText =
-    JSON.stringify(data);
-    // console.log(data.message)
-    let imagem = `
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            // console.log(data);
+            document.getElementById('json_aqui').innerText =
+                JSON.stringify(data);
+            // console.log(data.message)
+            let imagem = `
     <img class="rounded img-fluid" src="${data.message}" />
     `;
-    // Insere a imagem no elemento HTML com id imagem_aqui
-    document.querySelector('#imagem_aqui').innerHTML = imagem;
-    });
-    }
+            // Insere a imagem no elemento HTML com id imagem_aqui
+            document.querySelector('#imagem_aqui').innerHTML = imagem;
+        });
+}
+
+fetch(url).then("receber o JSON").then("Trata o JSON e mostrar as informações em tela para o usuario").catch("cactvh é para tratar erros retornados pela API")
